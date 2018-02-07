d08ed482934e044ce4f7e35bd4568ed3b0bc42c8
RC0028645 <RC0028645@CRO-ICF07-20>
2018-02-07 11:51:03 +0000

    uefhbfd

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -32,6 +32,14 @@
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // richTextBox2
@@ -47,7 +55,7 @@
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(330, 35);
+            this.comboBox3.Location = new System.Drawing.Point(125, 35);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
             this.comboBox3.TabIndex = 1;
@@ -57,7 +65,7 @@
             // 
             this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(117, 35);
+            this.comboBox4.Location = new System.Drawing.Point(329, 35);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(121, 21);
             this.comboBox4.TabIndex = 2;
@@ -73,14 +81,101 @@
             this.label2.Text = "label2";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 21);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "0";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(125, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(329, 82);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+//            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+//            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+//            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(125, 121);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 9;
+//            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(329, 121);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 10;
+//            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(0, 121);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(100, 21);
+            this.richTextBox3.TabIndex = 11;
+            this.richTextBox3.Text = "0";
+//            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(515, 262);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox4);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.richTextBox2);
             this.Name = "Form1";
+//            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +187,14 @@
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.ComboBox comboBox4;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.RichTextBox richTextBox3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -23,6 +23,30 @@ namespace WindowsFormsApp1
             {"Mm", 1e6 },
             {"Gm", 1e9 },
         };
+
+        Dictionary<string, double> areas = new Dictionary<string, double>()
+        {
+            {"nm²", 1e-18 },
+            {"um²", 1e-12 },
+            {"mm²", 1e-6 },
+            {"cm²", 1e-4 },
+            {"m²", 1 },
+            {"km²", 1e6 },
+            {"Mm²", 1e12 },
+            {"Gm²", 1e18 },
+        };
+
+        Dictionary<string, double> volumes = new Dictionary<string, double>()
+        {
+            {"nm³", 1e-27 },
+            {"um³", 1e-18 },
+            {"mm³", 1e-9 },
+            {"cm³", 1e-6 },
+            {"m³", 1 },
+            {"km³", 1e9 },
+            {"Mm³", 1e18 },
+            {"Gm³", 1e27 },
+        };
         public Form1()
         {
             InitializeComponent();
@@ -34,9 +58,24 @@ namespace WindowsFormsApp1
             comboBox3.SelectedIndex = 4;
             comboBox4.SelectedIndex = 4;
             base.Update();
-        }
+            foreach (string k in areas.Keys)
+            {
+                comboBox1.Items.Add(k);
+                comboBox2.Items.Add(k);
+            }
+            comboBox1.SelectedIndex = 4;
+            comboBox2.SelectedIndex = 4;
+            base.Update();
+            foreach (string k in volumes.Keys)
+            {
+                comboBox5.Items.Add(k);
+                comboBox6.Items.Add(k);
+            }
+            comboBox5.SelectedIndex = 4;
+            comboBox6.SelectedIndex = 4;
+            base.Update();
 
-        void update()
+            void update()
         {
             try
             {
@@ -47,7 +86,23 @@ namespace WindowsFormsApp1
             catch
             {
                 label2.Text = "";
-            }
+
+                    double d3 = double.Parse(richTextBox2.Text);
+                    double d4 = d3 * areas[comboBox1.Text] / areas[comboBox2.Text];
+                    label1.Text = d4.ToString();
+
+               
+
+                    double d5 = double.Parse(richTextBox2.Text);
+                    double d6 = d5 * volumes[comboBox5.Text] / volumes[comboBox6.Text];
+                    label3.Text = d6.ToString();
+      
+                    label2.Text = "";
+                }
+                catch
+                {
+                    label2.Text = "";
+                }
         }
         private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
@@ -67,6 +122,19 @@ namespace WindowsFormsApp1
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
             update();
+        }
+
+       
+      
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
